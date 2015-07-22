5dfea5b23a47fcc449b465158cbab9a2181643c4
Anar <Anar@Anars-MacBook-Pro.local>
2015-07-22 17:29:30 +0200

    Initial commit

---
+++ src/Test.java
@@ -0,0 +1,11 @@
+/**
+ * Created by Anar on 7/22/15.
+ */
+public class Test {
+
+
+    public static void main(String[] args){
+        System.out.println("Hello world");
+
+    }
+}
---
+++ src/XmlParser.java
@@ -0,0 +1,16 @@
+/**
+ * Created by Anar on 7/22/15.
+ */
+import org.w3c.dom.*;
+import javax.xml.parsers.*;
+import java.io.*;
+
+
+public class XmlParser {
+
+
+
+
+
+}
+
